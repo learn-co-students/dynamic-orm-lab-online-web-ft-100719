39624c83caa442c15f22ec0d2f05b7c0f46114a2
jacquibushfi <jacqui@stainedglassofmckinney.com>
2019-11-05 15:16:46 -0600

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -40,6 +40,7 @@ class InteractiveRecord
     DB[:conn].execute(sql)
 
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  self
   end
 
   def table_name_for_insert
