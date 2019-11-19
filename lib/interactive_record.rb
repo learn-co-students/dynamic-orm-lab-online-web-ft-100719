fe236b62678786c2a4f4f54055ced11c9c152085
lmjcbs <hello@liamjacobs.dev>
2019-11-19 21:43:45 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -43,4 +43,12 @@ class InteractiveRecord
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
 
+  def self.find_by_name(name)
+    DB[:conn].execute("SELECT * FROM #{table_name} WHERE name = ?", [name])
+  end
+
+  def self.find_by(att)
+    DB[:conn].execute("SELECT * FROM #{table_name} WHERE #{att.keys[0].to_s} = ?", [att.values[0]])
+  end
+
 end
--- lib/student.rb
+++ lib/student.rb
@@ -1,10 +1,9 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'interactive_record.rb'
-require'pry'
+
 class Student < InteractiveRecord
 
-  #binding.pry
   self.column_names.each {|col_name| attr_accessor col_name.to_sym}
 
 end
